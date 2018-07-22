7869d178a74ed1a06dd352131ced28cc35329e82
Joe Kowal <3199871+JoeKowal@users.noreply.github.com>
2018-07-22 11:35:58 -0400

    bugfixes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,42 @@
-/*
- * Create a list that holds all of your cards
- */
+//hold all cards
+let card = document.getElementsByClassName("card");
+let allCards = [...card];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+//init
+let timer;
+let openCards = []; //opened cards
+let matchedCards = []; //matched Cards
+let firstClick = null; //1st click
+let currentClick = null; //2nd click
+let firstClickIcon = null; //1st click picture
+let currentClickIcon = null; //2nd click picture
+let shuffledCards;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let timerOn = false; //no timer until first move
+let timeOutput;
+const deck = document.querySelector(".deck");
+let winStars;
+let moveCounter = 0; //# of moves
+
+//shuffle
+window.onload = function() {
+  shuffleCards();
+};
+
+function shuffleCards() {
+  shuffledCards = shuffle(allCards);
+  for (let i = 0; i < shuffledCards.length; i++) {
+    deck.appendChild(shuffledCards[i]);
+  }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
   var currentIndex = array.length,
     temporaryValue,
     randomIndex;
-
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -22,157 +44,192 @@ function shuffle(array) {
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
 }
 
-let numMoves = 0;
-
-const deck = document.querySelector(".deck");
-
-let flippedCards = []; //to hold clicked cards
-
-let clockOff = true;
-
-let time = 0;
-
-let clockId;
-
-const minutes = Math.floor(time / 60);
-const seconds = time % 60;
-
-//shuffle
-function shuffleCards() {
-  const unshuffled = Array.from(document.querySelectorAll(".deck li"));
-  const shuffled = shuffle(unshuffled);
-  for (card of shuffled) {
-    deck.appendChild(card);
-  }
-}
-
-shuffleCards();
-
-
-
-//count number of turns
-function addTurn() {
-  numMoves++;
-  const turnText = document.querySelector("numMoves");
-  turnText.innerHTML = numMoves;
-}
-
-//too many moves = lose stars
-function takeAwayStar() {
-  const starDisplay = document.querySelectorAll(".stars li");
-  for (star of starDisplay) {
-    if (star.style.display !== "none") {
-      star.style.display = "none";
-      break;
-    }
-  }
-}
-
-//how many stars did you get?
-function countMoves() {
-  if (numMoves === 20 || numMoves === 25) {
-    takeAwayStar();
-  }
-}
-
-//flip the card
-function flipCard(clickCard) {
-  clickCard.classList.toggle("open");
-  clickCard.classList.toggle("show");
-}
-
-//add to array
-function addClickCard(clickCard) {
-  flippedCards.push(clickCard);
-}
-
-//is it a match?
-function doTheyMatch() {
-  if (
-    flippedCards[0].firstelementchild.classname ===
-    flippedCards[1].firstelementchild.classname
-  ) {
-    flippedCards[0].classlist.toggle("match");
-    flippedCards[1].classlist.toggle("match");
-    flippedCards = [];
-  } else {
-    setTimeout(() => {
-      //pause to see card here
-      flipCard(flippedCards[0]);
-      flipCard(flippedCards[1]);
-      flippedCards = [];
-    }, 1450);
-  }
-}
-
-//is this a real valid click
-function isAGoodClick(clickCard) {
-  return (
-    clickCard.classlist.contains("card") &&
-    !clickCard.classlist.contains("match") &&
-    flippedCards.length < 2 &&
-    !flippedCards.includes(clickCard)
-  );
-}
-
-//count game time with clock
-function keepTime() {
-  let clockId = setTimeout(() => {
-   time++;
-   displayTime(); 
-  }, 997);
-}
-
-function stopClock() {
-  clearInterval(clockId);
-}
-
-function showTime() {
-  const clock = document.querySelector('.clock');
-  if (seconds < 10) {
-clock.innerHTML = `${minutes}:0${seconds}`;
-  } else {
-    clock.innerHTML = `${minutes}:${seconds}`;
-  }
-}
+//event listeners
+document.querySelector(".deck").addEventListener("click", clickedCard);
+document.querySelector(".restart").addEventListener("click", reset);
+document.querySelector(".btn-primary").addEventListener("click", reset);
 
 //flip card when clicked
-deck.addEventListener("click", event => {
-  const clickCard = event.target;
-  if (clickCard.classList.contains("card")) {
-    flipCard(clickCard);
+function clickedCard() {
+  if (timerOn === false) {
+    timerOn = true;
+    setTimer();
   }
-});
+  currentClick = event.target;
+  if (currentClick.classList.contains("match")) {
+    //do nothing this card is already matched
+  } else if (currentClick.classList.contains("card")) {
+    //show card and disable it from being clicked twice
+    currentClick.classList.add("open", "show-card", "disabled");
+    CurrentClickIcon = currentClick.lastElementChild.className;
+    openedCard();
+  }
+}
 
-//add clicked cards to array
-deck.addEventListener("click", event => {
-  const clickCard = event.target;
-  if (isAGoodClick(clickCard)) {
-    flipCard(clickCard);
-    addClickCard(clickCard);
-    if (clockOff) {
-      keepTime();
-      clockOff = false;
+//compare cards
+function openedCard() {
+  openCards.push(CurrentClickIcon);
+  if (openCards.length === 1) {
+    firstClick = currentClick;
+    firstClickIcon = CurrentClickIcon;
+  }
+  //2 cards = update number of moves
+  if (openCards.length === 2) {
+    updateMoves();
+    //do the 2 cards match
+    if (openCards[0] === openCards[1]) {
+      //if yes then match function
+      isAMatch();
+    } else {
+      //if no then not a match function
+      notAMatch();
     }
-    if (flippedCards.length === 2) {
-      doTheyMatch(clickCard);
-      addTurn();
-      countMoves();
+    openCards = [];
+  }
+}
+
+//have all cards have been matched
+function isAMatch() {
+  currentClick.classList.add("match");
+  firstClick.classList.add("match");
+  matchedCards.push(currentClickIcon);
+  matchedCards.push(firstClickIcon);
+  if (matchedCards.length === 16) {
+    //all done = winning
+    win();
+  }
+}
+
+function notAMatch() {
+  document.querySelector(".deck").removeEventListener("click", clickedCard);
+  setTimeout(function() {
+    currentClick.classList.remove("open", "show-card", "disabled");
+    firstClick.classList.remove("open", "show-card", "disabled");
+    document.querySelector(".deck").addEventListener("click", clickedCard);
+  }, 600);
+}
+
+function updateMoves() {
+  moveCounter++;
+  document.querySelector(".moves").innerHTML = "Moves: " + moveCounter;
+  rating();
+}
+
+function win() {
+  clearTimer();
+  generateWinStars();
+  document.querySelector(".modal-body").lastElementChild.innerHTML =
+    "Moves Made: " +
+    moveCounter +
+    "<br />" +
+    timeOutput +
+    "<br />" +
+    "Rating: " +
+    winStars;
+  $("#winModal").modal("show");
+}
+
+//reset the game
+function reset() {
+  clearTimer();
+  for (let e of document.querySelectorAll(".stars"))
+    e.children[2].style.visibility = "visible";
+  for (let e of document.querySelectorAll(".stars"))
+    e.children[1].style.visibility = "visible";
+  moveCounter = 0;
+  firstClick = null;
+  currentClick = null;
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  document.querySelector(".myTimer").innerHTML = "Time Taken: 0 secs";
+  timerOn = false;
+  document.querySelector(".moves").innerHTML = "Moves: " + moveCounter;
+  let cards = document.getElementsByClassName("card");
+  let cardsEl;
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl = cards[i].lastElementChild.className;
+    if (openCards.includes(cardsEl)) {
+      cards[i].classList.remove("open", "show-card", "disabled");
+    } else if (matchedCards.includes(cardsEl)) {
+      cards[i].classList.remove("open", "show-card", "match", "disabled");
     }
   }
-});
+  openCards = [];
+  matchedCards = [];
+  shuffleCards();
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//calculate time
+function setTimer() {
+  timer = setInterval(function() {
+    if (seconds === 60) {
+      minutes++;
+      seconds = 0;
+    }
+    if (minutes === 60) {
+      hours++;
+      minutes = 0;
+    }
+    seconds++;
+    timerOutput();
+  }, 1000);
+}
+
+//stop time
+function clearTimer() {
+  clearInterval(timer);
+}
+
+//timer for scorecard
+function timerOutput() {
+  //hours
+  if (hours === 1) {
+    timeOutput = "Time Taken: " + hours + " hr ";
+  } else if (hours > 1) {
+    timeOutput = "Time Taken: " + hours + " hrs ";
+  } else if (hours === 0) {
+    timeOutput = "Time Taken: ";
+  }
+  //minutes
+  if (minutes === 0 && hours === 0) {
+  } else if (minutes === 1) {
+    timeOutput += minutes + " min ";
+  } else if (minutes != 1) {
+    timeOutput += minutes + " mins ";
+  }
+  //seconds
+  if (seconds === 1) {
+    timeOutput += seconds + " sec";
+  } else {
+    timeOutput += seconds + " secs";
+  }
+  document.querySelector(".myTimer").innerHTML = timeOutput;
+}
+
+//how many stars to show
+function rating() {
+  if (moveCounter < 13) {
+    //3 stars
+  } else if (moveCounter >= 14 && moveCounter < 18) {
+    for (let e of document.querySelectorAll(".stars"))
+      e.children[2].style.visibility = "hidden";
+  } else if (moveCounter >= 19) {
+    for (let e of document.querySelectorAll(".stars"))
+      e.children[1].style.visibility = "hidden";
+  }
+}
+
+//code to show on win screen
+function generateWinStars() {
+  if (moveCounter < 11) {
+    winStars = `<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>`;
+  } else if (moveCounter >= 11 && moveCounter < 20) {
+    winStars = `<i class="fa fa-star"></i><i class="fa fa-star"></i>`;
+  } else if (moveCounter >= 20) {
+    winStars = `<i class="fa fa-star"></i>`;
+  }
+}
